93597e68a3bf6dabbe627048071d04fa6012976b
LeeQuiett <vkdnjpo1512@naver.com>
2024-07-23 13:01:10 +0900

    간단간단간단연결리스트

---
+++ dataStruct01.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct USERDATA {
+	int age;
+	char name[32];
+	char phone[32];
+	struct USERDATA* pUserData;
+} USERDATA;
+
+int main(void) {
+	USERDATA user[4] = {
+		{12, "lob", "010-1234-5678", NULL},
+		{12, "onA", "010-1234-5678", NULL},
+		{12, "beat", "010-1234-5678", NULL},
+		{12, "Hecop", "010-1234-5678", NULL},
+	};
+
+	user[0].pUserData = &user[1];
+	user[1].pUserData = &user[2];
+	user[2].pUserData = &user[3];
+	
+	printf("%s\n", user[0].pUserData->name);
+	
+}
